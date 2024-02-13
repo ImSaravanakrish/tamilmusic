f4b054971a8693d30ca21ba781f0e34790207685
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:07:32 +0530

    Update live.py

--- TamiliniMusic/plugins/play/live.py
+++ TamiliniMusic/plugins/play/live.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.stream.stream import stream
+from TamiliniMusic import YouTube, app
+from TamiliniMusic.utils.channelplay import get_channeplayCB
+from TamiliniMusic.utils.decorators.language import languageCB
+from TamiliniMusic.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
