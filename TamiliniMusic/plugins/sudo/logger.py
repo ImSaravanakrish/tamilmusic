349654986db78a0dd74bebc0b0be1162281ce739
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:10:01 +0530

    Update logger.py

--- TamiliniMusic/plugins/sudo/logger.py
+++ TamiliniMusic/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_off, add_on
-from AnonXMusic.utils.decorators.language import language
+from TamiliniMusic import app
+from TamiliniMusic.misc import SUDOERS
+from TamiliniMusic.utils.database import add_off, add_on
+from TamiliniMusic.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
