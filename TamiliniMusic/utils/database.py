d4141533955457be147a1147bf5f0c7244ef2bb2
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 20:20:41 +0530

    Update database.py

--- TamiliniMusic/utils/database.py
+++ TamiliniMusic/utils/database.py
@@ -1,8 +1,8 @@
 import random
 from typing import Dict, List, Union
 
-from AnonXMusic import userbot
-from AnonXMusic.core.mongo import mongodb
+from TamiliniMusic import userbot
+from TamiliniMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
