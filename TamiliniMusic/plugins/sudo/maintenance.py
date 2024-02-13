5a925d5de49f8d28894bce7965fdcfcf26ff682e
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:10:31 +0530

    Update maintenance.py

--- TamiliniMusic/plugins/sudo/maintenance.py
+++ TamiliniMusic/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from TamiliniMusic import app
+from TamiliniMusic.misc import SUDOERS
+from TamiliniMusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
