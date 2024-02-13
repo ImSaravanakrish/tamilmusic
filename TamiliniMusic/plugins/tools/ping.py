e189c447217edae17aaaa7d1b749ee8785272a3c
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:12:30 +0530

    Update ping.py

--- TamiliniMusic/plugins/tools/ping.py
+++ TamiliniMusic/plugins/tools/ping.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import bot_sys_stats
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.inline import supp_markup
+from TamiliniMusic import app
+from TamiliniMusic.core.call import Anony
+from TamiliniMusic.utils import bot_sys_stats
+from TamiliniMusic.utils.decorators.language import language
+from TamiliniMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
