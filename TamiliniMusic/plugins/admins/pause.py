b31b86d42f273c6f23e67fc78e6d2eaf938cdf0b
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:00:04 +0530

    Update pause.py

--- TamiliniMusic/plugins/admins/pause.py
+++ TamiliniMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from TamiliniMusic import app
+from TamiliniMusic.core.call import Anony
+from TamiliniMusic.utils.database import is_music_playing, music_off
+from TamiliniMusic.utils.decorators import AdminRightsCheck
+from TamiliniMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
