a4e3db68192d0927ccfb26db27392df4e82cb627
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:06:08 +0530

    Update broadcast.py

--- TamiliniMusic/plugins/misc/broadcast.py
+++ TamiliniMusic/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from TamiliniMusic import app
+from TamiliniMusic.misc import SUDOERS
+from TamiliniMusic.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.formatters import alpha_to_int
+from TamiliniMusic.utils.decorators.language import language
+from TamiliniMusic.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
