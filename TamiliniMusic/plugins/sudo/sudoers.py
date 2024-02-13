9ce98b5eb54747131d6adb67b61a82f2a831098a
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:11:08 +0530

    Update sudoers.py

--- TamiliniMusic/plugins/sudo/sudoers.py
+++ TamiliniMusic/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from TamiliniMusic import app
+from TamiliniMusic.misc import SUDOERS
+from TamiliniMusic.utils.database import add_sudo, remove_sudo
+from TamiliniMusic.utils.decorators.language import language
+from TamiliniMusic.utils.extraction import extract_user
+from TamiliniMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
