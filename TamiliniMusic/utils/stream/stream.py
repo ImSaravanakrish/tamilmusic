f618e77c621ed9f1dac7b9002913e96345abd5e0
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 20:28:48 +0530

    Update stream.py

--- TamiliniMusic/utils/stream/stream.py
+++ TamiliniMusic/utils/stream/stream.py
@@ -6,14 +6,14 @@ from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from AnonXMusic import Carbon, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import AnonyBin
-from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
-from AnonXMusic.utils.thumbnails import get_thumb
+from TamiliniMusic.core.call import Anony
+from TamiliniMusic.misc import db
+from TamiliniMusic.utils.database import add_active_video_chat, is_active_chat
+from TamiliniMusic.utils.exceptions import AssistantErr
+from TamiliniMusic.utils.inline import aq_markup, close_markup, stream_markup
+from TamiliniMusic.utils.pastebin import AnonyBin
+from TamiliniMusic.utils.stream.queue import put_queue, put_queue_index
+from TamiliniMusic.utils.thumbnails import get_thumb
 
 
 async def stream(
