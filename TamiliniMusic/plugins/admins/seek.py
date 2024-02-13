7dcce1e3f389583556f33834bc6066a5749722bd
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-13 15:00:58 +0530

    Update seek.py

--- TamiliniMusic/plugins/admins/seek.py
+++ TamiliniMusic/plugins/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AdminRightsCheck, seconds_to_min
-from AnonXMusic.utils.inline import close_markup
+from TamiliniMusic import YouTube, app
+from TamiliniMusic.core.call import Anony
+from TamiliniMusic.misc import db
+from TamiliniMusic.utils import AdminRightsCheck, seconds_to_min
+from TamiliniMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
