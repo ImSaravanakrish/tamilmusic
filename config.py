8e387d84488ce4cf4d02dc08d932a3ad48bba9a1
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2024-02-14 14:55:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -87,12 +87,12 @@ PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
 TELEGRAM_VIDEO_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+STREAM_IMG_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/73fe5d067214726f6defd.jpg"
 
 
 def time_to_seconds(time):
